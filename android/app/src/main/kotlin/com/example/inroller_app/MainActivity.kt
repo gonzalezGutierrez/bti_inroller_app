56af478451fc55c644cfdabbeeba98f53f0838c9
Jesús Alberto GONZALEZ GUTIERREZ <163174@ids.upchiapas.edu.mx>
2020-09-01 07:37:16 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/inroller_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inroller_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/api_url.dart
@@ -0,0 +1,1 @@
+final String api_ur = 'http://192.168.1.68:8000/api/';
